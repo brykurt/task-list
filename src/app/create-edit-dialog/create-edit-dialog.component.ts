5ec376863cc5b16281fe5a8003cf6789804befb1
Bryon Kurt Segunto <bryonkurts@gmail.com>
2026-01-10 14:30:44 +0800

    Initial commit for the create and edit dialog.

---
+++ src/app/create-edit-dialog/create-edit-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-edit-dialog',
+  templateUrl: './create-edit-dialog.component.html',
+  styleUrls: ['./create-edit-dialog.component.scss']
+})
+export class CreateEditDialogComponent {
+
+}
